0eb1eee8a02a2b4760cf8235bf31d584f32a2527
rossanaferreira87 <33179725+rossanaferreira87@users.noreply.github.com>
2019-10-24 17:42:27 +0200

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,6 @@ config :tumblrx, Tumblrx.Repo,
   username: "postgres",
   password: "",
   hostname: "localhost",
-  # ssl: true,
-  # ssl_opts: [
-  #   cacertfile: "priv/AmazonRootCA1.pem"
-  # ],
   show_sensitive_data_on_connection_error: true,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
